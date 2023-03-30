35e305393704f29198d41e544e5f6dc06eff0c82
Andy Lau <andyjavaandy@gmail.com>
2023-03-31 07:29:56 +0800

    modify

--- grid/index.py
+++ grid/index.py
@@ -1,47 +1,7 @@
-'''
-(最初寫法)專案在學習grid的編排
-
-
 import tkinter as tk
 from tkinter import ttk
-
-class Window(tk.Tk):
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
-        ttkStyle = ttk.Style()
-        ttkStyle.configure('back.TFrame',background='blue')
-        
-        mainFrame = ttk.Frame(self,style='back.TFrame')        
-        mainFrame.pack(expand=True,fill=tk.BOTH)
-        
-
-        topFrame = ttk.Frame(mainFrame,style='back.TFrame',height=100)
-        topFrame.pack(fill=tk.X)
-
-        bottomFrame = ttk.Frame(mainFrame,style='back.TFrame')
-        bottomFrame.pack(expand=True,fill=tk.BOTH)
-
-
-def main():
-    
-    這是程式的執行點
-
-    window = Window()
-    window.title("BMI計算")
-    window.geometry("400x500")
-    window.mainloop()
-
-if __name__ == "__main__":
-    main()
-'''
-
-      
-'''
-專案在學習grid的編排
-'''
-
-import tkinter as tk
-from tkinter import ttk
+import re
+from PIL import Image,ImageTk
 
 class Window(tk.Tk):
     def __init__(self,**kwargs):
@@ -52,66 +12,170 @@ class Window(tk.Tk):
         ttkStyle.configure('white.TFrame',background='#ffffff')
         ttkStyle.configure('yellow.TFrame',background='yellow')
         ttkStyle.configure('white.TLabel',background='#ffffff')
-        ttkStyle.configure('gridLabel.TLabel',font=('Helvetica', 16),foreground="#666666")
+        ttkStyle.configure('gridLabel.TLabel',font=('Helvetica', 16),foreground='#666666')
         ttkStyle.configure('gridEntry.TEntry',font=('Helvetica', 16))
         
         mainFrame = ttk.Frame(self)        
-        mainFrame.pack(expand=True,fill=tk.BOTH)
         mainFrame.pack(expand=True,fill=tk.BOTH,padx=30,pady=30)
+        
 
         topFrame = ttk.Frame(mainFrame,height=100)
         topFrame.pack(fill=tk.X)
 
-        ttk.Label(topFrame,text="BMI試算",font=('Helvetica', '20')).pack(pady=20)
+        ttk.Label(topFrame,text="BMI試算",font=('Helvetica', '20')).pack(pady=(80,20))
 
-        
         bottomFrame = ttk.Frame(mainFrame)
         bottomFrame.pack(expand=True,fill=tk.BOTH)
-       
-
         bottomFrame.columnconfigure(0,weight=3,pad=20)
         bottomFrame.columnconfigure(1,weight=5,pad=20)
-        bottomFrame.rowconfigure(0,weight=1,pad=20)
-        bottomFrame.rowconfigure(3,weight=1,pad=20)
-        bottomFrame.rowconfigure(4,weight=1,pad=20)
-        bottomFrame.rowconfigure(5,weight=1,pad=20)
-        bottomFrame.rowconfigure(6,weight=1,pad=20)
+        bottomFrame.rowconfigure(0, weight=1,pad=20)
+        bottomFrame.rowconfigure(3, weight=1,pad=20)
+        bottomFrame.rowconfigure(4, weight=1,pad=20)
+        bottomFrame.rowconfigure(5, weight=1,pad=20)
+        bottomFrame.rowconfigure(6, weight=1,pad=20)
+
+        #定義entry的textvariable
+        self.nameStringVar = tk.StringVar()
+        self.birthStringVar = tk.StringVar()
+        self.heightIntVar = tk.IntVar()
+        self.weightIntVar = tk.IntVar()
+
         ttk.Label(bottomFrame,text="姓名:",style='gridLabel.TLabel').grid(column=0,row=0,sticky=tk.E)
-        nameEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry')
+        
+        nameEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry',textvariable=self.nameStringVar)
         nameEntry.grid(column=1,row=0,sticky=tk.W,padx=10)
 
         ttk.Label(bottomFrame,text="出生年月日:",style='gridLabel.TLabel').grid(column=0,row=1,sticky=tk.E)
         ttk.Label(bottomFrame,text="(2000/03/01)",style='gridLabel.TLabel').grid(column=0,row=2,sticky=tk.E)
 
-        birthEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry')
+        birthEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry',textvariable=self.birthStringVar)
         birthEntry.grid(column=1,row=1,sticky=tk.W,rowspan=2,padx=10)
 
         ttk.Label(bottomFrame,text="身高(cm):",style='gridLabel.TLabel').grid(column=0,row=3,sticky=tk.E)
-        heightEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry')
+        heightEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry',textvariable=self.heightIntVar)
         heightEntry.grid(column=1,row=3,sticky=tk.W,padx=10)
 
         ttk.Label(bottomFrame,text="體重(kg):",style='gridLabel.TLabel').grid(column=0,row=4,sticky=tk.E)
-        weightEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry')
-        weightEntry.grid(column=1,row=4,sticky=tk.W)
+        weightEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry',textvariable=self.weightIntVar)
         weightEntry.grid(column=1,row=4,sticky=tk.W,padx=10)
 
-        messageText = tk.Text(bottomFrame,height=5,width=35,state=tk.DISABLED,takefocus=0,bd=0)
-        messageText.grid(column=0,row=5,sticky=tk.N+tk.S,columnspan=2)
+        self.messageText = tk.Text(bottomFrame,height=5,width=35,state=tk.DISABLED,takefocus=0,bd=0)
+        self.messageText.grid(column=0,row=5,sticky=tk.N+tk.S,columnspan=2)
 
-        commitBtn = ttk.Button(bottomFrame,text="計算")
-        commitBtn.grid(column=1,row=6,sticky=tk.W)
+        #---------commitFrame開始--------------------
+        #有左右2個按鈕
+        #
+        commitFrame = ttk.Frame(bottomFrame)
+        commitFrame.grid(column=0,row=6,columnspan=2)
+        commitFrame.columnconfigure(0,pad=10)
+        commitBtn = ttk.Button(commitFrame,text="計算",command=self.press_commit)
+        commitBtn.grid(column=0,row=0,sticky=tk.W)
+
+        clearBtn = ttk.Button(commitFrame,text="清除",command=self.press_clear)
+        clearBtn.grid(column=1,row=0,sticky=tk.E)
+        #---------commitFrame結束--------------------
+
+        #---------建立Logo--------------------
+        logoImage = Image.open('logo.png')
+        resizeImage = logoImage.resize((180,45),Image.LANCZOS)
+        self.logoTkimage = ImageTk.PhotoImage(resizeImage)
+        logoLabel = ttk.Label(self,image=self.logoTkimage,width=180)
+        logoLabel.place(x=40,y=45)
+
+    def press_clear(self) -> None:
+        self.nameStringVar.set("")
+        self.birthStringVar.set("")
+        self.heightIntVar.set(0)
+        self.weightIntVar.set(0)
+        self.messageText.configure(state=tk.NORMAL)
+        self.messageText.delete("1.0",tk.END)
+        self.messageText.configure(state=tk.DISABLED)
+        print("清除")
+
+    def press_commit(self) -> None:
+        self.check_data()
+        def get_zodiac_sign(day, month):
+    # 檢查輸入的日期是否有效
+            if month == 12:
+                astro_sign = 'Sagittarius' if (day < 22) else 'Capricorn'
+            elif month == 1:
+                astro_sign = 'Capricorn' if (day < 20) else 'Aquarius'
+            elif month == 2:
+                astro_sign = 'Aquarius' if (day < 19) else 'Pisces'
+            elif month == 3:
+                astro_sign = 'Pisces' if (day < 21) else 'Aries'
+            elif month == 4:
+                astro_sign = 'Aries' if (day < 20) else 'Taurus'
+            elif month == 5:
+                astro_sign = 'Taurus' if (day < 21) else 'Gemini'
+            elif month == 6:
+                stro_sign = 'Gemini' if (day < 21) else 'Cancer'
+            elif month == 7:
+                astro_sign = 'Cancer' if (day < 23) else 'Leo'
+            elif month == 8:
+                astro_sign = 'Leo' if (day < 23) else 'Virgo'
+            elif month == 9:
+                astro_sign = 'Virgo' if (day < 23) else 'Libra'
+            elif month == 10:
+                astro_sign = 'Libra' if (day < 23) else 'Scorpio'
+            elif month == 11:
+                astro_sign = 'Scorpio' if (day < 22) else 'Sagittarius'
+            return astro_sign
+            star = astro_sign()
+    def check_data(self) -> None:
+        dateRegex = re.compile(r"^\d\d\d\d/\d\d/\d\d$")
+        nameValue = self.nameStringVar.get()
+        birthValue = self.birthStringVar.get()
+        birthMatch = re.match(dateRegex,birthValue)
+        if  birthMatch is None:            
+            birthValue = ""
+
+        try:
+            heightValue = self.heightIntVar.get()
+        except:
+            heightValue = 0
+
+        try:
+            weightValue = self.weightIntVar.get()
+        except:
+            weightValue =  0
+        
+        if nameValue == "" or birthValue=="" or heightValue==0 or weightValue==0 :            
+            self.messageText.configure(state=tk.NORMAL)
+            self.messageText.delete("1.0",tk.END)
+            self.messageText.insert(tk.END,"有欄位沒填或格式不正確")
+            self.messageText.configure(state=tk.DISABLED)
+        else:
+            bmi = weightValue / (heightValue / 100) ** 2
+            message = f"{nameValue}您好:\n"
+            message += f"出生年月日:{birthValue}\n,你的星座是{star}"
+            message += f"BMI值是:{bmi:.2f}\n"
+            message += f"狀態是:xxxx"
+        
+    
+
+        self.messageText.configure(state=tk.NORMAL)
+        self.messageText.delete("1.0",tk.END)
+        self.messageText.insert(tk.END,message)
+        self.messageText.configure(state=tk.DISABLED)
+
+    
 
 
 
+def close_window(w):
+    w.destroy()        
 
 
 def main():
     '''
     這是程式的執行點
-    '''
+    ''' 
+
     window = Window()
     window.title("BMI計算")
-    #window.geometry("400x500")
+    window.resizable(width=False, height=False)
+    window.protocol("WM_DELETE_WINDOW",lambda:close_window(window))
     window.mainloop()
 
 if __name__ == "__main__":
