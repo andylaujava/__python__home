1019b89c0ea51bcc9fa2cd5c4c642034648f4d66
Andy Lau <andyjavaandy@gmail.com>
2023-03-31 14:15:27 +0800

    modify

---
+++ drawing/index2.py
@@ -0,0 +1,122 @@
+import tkinter as tk
+from tkinter import ttk
+
+class DrawingWindow(tk.Tk):
+    
+   
+    def __init__(self, **kwargs):
+        
+       
+    super().__init__(**kwargs)
+    self.title("畫圖")
+        
+        drawing_frame = ttk.LabelFrame(self, text=
+        
+        drawing_frame = ttk.LabelFrame(self, text
+
+        
+        drawing_frame = ttk.LabelFrame(self,
+
+        
+        drawing_frame = ttk.LabelFrame(self
+
+        
+        drawing_frame = ttk.LabelFrame
+
+        
+        drawing_frame = ttk.Label
+
+        
+        drawing_frame = ttk
+
+        
+        drawing_frame =
+
+        
+        drawing_frame
+
+        
+        drawing
+
+        
+       
+
+        
+'這裡是畫圖區')
+        drawing_frame.pack(expand=
+        drawing_frame.pack(expand
+
+        drawing_frame.pack(exp
+
+        drawing_frame.pack
+
+        drawing_frame
+
+        drawing
+
+       
+True, padx=20, pady=20)
+        
+        tk.Button(drawing_frame, text=
+        
+        tk.Button(drawing_frame, text
+
+        
+        tk.Button(drawing_frame,
+
+        
+        tk.Button(drawing_frame
+
+        
+        tk.Button(drawing
+
+        
+        tk.Button(d
+
+        
+        tk.Button
+
+        
+        tk
+
+        
+       
+
+        
+"Press Me").pack(padx=10, pady=10, ipadx=20, ipady=10)
+
+
+def main():
+    window = DrawingWindow()
+    window.mainloop()
+
+
+    window = DrawingWindow()
+    window.mainloop
+
+    window = DrawingWindow()
+    window
+
+    window = DrawingWindow()
+   
+
+    window = DrawingWindow()
+
+    window = DrawingWindow
+
+    window = Drawing
+
+    window =
+
+    window
+
+   
+if __name__ == "__main__":
+    main()
+
+    main()
+``
+
+    main()
+
+    main
