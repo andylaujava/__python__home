810ec22d3e53170b0be79e2d176ee71bcc349b1f
Andy Lau <andyjavaandy@gmail.com>
2023-04-07 16:52:36 +0800

    modify

--- widgets/parts.py
+++ widgets/parts.py
@@ -77,3 +77,62 @@ class MedianFrame(ttk.LabelFrame):
         print(self.checkStringVar.get())
         #self.w.radioButtonEventOfMedianFrame(self.checkStringVar.get())
     
+class BottomFrame(ttk.LabelFrame):
+    def __init__(self,master,**kwargs):
+        super().__init__(master,**kwargs)
+        self.w = master
+        listFrame = ttk.LabelFrame(self,text="List Box")
+        listFrame.pack(side=tk.LEFT,padx=10,pady=10) 
+        list = tk.Listbox(listFrame,height=6,width=10)
+        list.pack(side=tk.LEFT)
+        self.data = []
+        for month in range(1,13):
+            list.insert(tk.END,f"{month}月")
+
+        
+
+        for item in self.data:
+            list.insert(tk.END,item)
+
+        scrollBar = ttk.Scrollbar(listFrame,command=list.yview)
+        scrollBar.pack(side=tk.RIGHT,fill=tk.Y)
+        list.configure(yscrollcommand=scrollBar.set)
+        list.bind('<<ListboxSelect>>', self.items_selected)
+
+        comboBoxFrame = ttk.LabelFrame(self,text="Combo Box")
+        comboBoxFrame.pack(side=tk.LEFT,fill=tk.Y,padx=10,pady=10)
+        self.comboBoxValues  = ('請選擇月份',
+                        'January',
+						'February',
+						'March',
+						'April',
+						'May',
+						'June',
+						'July',
+						'August',
+						'September',
+						'October',
+						'November',
+						'December')
+
+        
+
+        comboBox = ttk.Combobox(comboBoxFrame,state= "readonly",width=8)
+        comboBox.pack()
+        comboBox.pack()
+        comboBox['values'] = self.comboBoxValues
+
+        def month_changed(self,event):
+            combobox = event.widget
+            selectedIndex= combobox.current()
+            selectedValue = self.comboBoxValues[selectedIndex]
+            self.w.comboBoxEventOfBottomFrame(selectedValue)        
+            comboBox.current(0)
+            comboBox.bind('<<ComboboxSelected>>', self.month_changed)
+
+
+        comboBox.pack(side=tk.RIGHT)
+
+    def items_selected(self,event):
+        listbox = event.widget
+        #selectedIndex -> tuple
